10c3f2ef1f5ee21963cf2c6d6b8a618027ff9a36
LordPrinz <plazinski2902@gmail.com>
2025-06-06 15:04:53 +0200

    Create gun

---
+++ Source/dzajlorant/Private/DGun.cpp
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "dzajlorant/Public/DGun.h"
+#include "Components/SkeletalMeshComponent.h"
+
+// Sets default values
+ADGun::ADGun()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	Root = CreateDefaultSubobject<USceneComponent>("Root");
+	SetRootComponent(Root);
+
+	SkeletalMesh = CreateDefaultSubobject<USkeletalMeshComponent>("Mesh");
+	SkeletalMesh->SetupAttachment(Root);
+}
+
+// Called when the game starts or when spawned
+void ADGun::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ADGun::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
