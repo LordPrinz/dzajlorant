949f30a09ebd0e6df0a6a4dd3bb2483d77032827
LordPrinz <plazinski2902@gmail.com>
2025-06-07 23:07:06 +0200

    Create AI task board

--- Source/dzajlorant/Private/ShooterAIController.cpp
+++ Source/dzajlorant/Private/ShooterAIController.cpp
@@ -3,3 +3,38 @@
 
 #include "ShooterAIController.h"
 
+#include "BehaviorTree/BlackboardComponent.h"
+#include "Kismet/GameplayStatics.h"
+
+void AShooterAIController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (BehaviorTree != nullptr)
+	{
+		RunBehaviorTree(BehaviorTree);
+
+		APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
+		
+		GetBlackboardComponent()->SetValueAsVector("PlayerLocation", PlayerPawn->GetActorLocation());
+		GetBlackboardComponent()->SetValueAsVector("StartLocation", GetPawn()->GetActorLocation());
+	}
+}
+
+void AShooterAIController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
+	
+	if (LineOfSightTo(PlayerPawn))
+	{
+		GetBlackboardComponent()->SetValueAsVector("PlayerLocation", PlayerPawn->GetActorLocation());
+		GetBlackboardComponent()->SetValueAsVector("LastKnownPlayerLocation", PlayerPawn->GetActorLocation());
+	} else
+	{
+		GetBlackboardComponent()->ClearValue("PlayerLocation");
+	}
+	
+
+
+}
