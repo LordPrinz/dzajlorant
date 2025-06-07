466e479f9d50ad53f8e9dabfe2b0a95c3a6e72a8
LordPrinz <plazinski2902@gmail.com>
2025-06-07 18:34:48 +0200

    Add AI controller template

---
+++ Source/dzajlorant/Private/ShooterAIController.cpp
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ShooterAIController.h"
+
+
+// Sets default values
+AShooterAIController::AShooterAIController()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void AShooterAIController::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AShooterAIController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
